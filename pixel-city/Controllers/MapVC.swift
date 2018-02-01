e1d8a0285596e35aacbe7cee726381610e4c7fae
ViXette <v.xette@gmail.com>
2018-02-01 20:29:48 +1000

    MapVC Interface

--- pixel-city/Controllers/MapVC.swift
+++ pixel-city/Controllers/MapVC.swift
@@ -3,13 +3,28 @@
 //
 
 import UIKit
+import MapKit
 
 
 class MapVC: UIViewController {
-
+	
+	@IBOutlet weak var mapView: MKMapView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+		mapView.delegate = self
+	}
+	
+
+	@IBAction func centerMapButtonTapped(_ sender: UIButton) {
+		
 	}
 
+}
+
+
+
+extension MapVC: MKMapViewDelegate {
+	
 }
